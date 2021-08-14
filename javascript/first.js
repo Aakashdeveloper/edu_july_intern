c65fa5e5e9d3a22c5b8fc304955b757f04f55c31
aakashdeveloper <ahanda205@gmail.com>
2021-08-14 15:38:56 +0530

    javascript

---
+++ javascript/first.js
@@ -0,0 +1,133 @@
+string ="hii" 'yes' "test" "66" '335' '3543ffefe' `Hiii`
+number= 87 8966786 88.4664645 .443
+boolean= true false
+
+es5 > Ecma Script5
+es6 > Ecma Script6
+
+var a = 10
+var b = 20
+a+b (addition)
+30
+
+var a = "Hii"
+var b = "JavaScript"
+a+b (concat)
+"HiiJavaScript"
+
+var a = 10
+var b = 20
+a-b
+-10
+b-a
+10
+a*b
+200
+a/b
+0.5
+b/a
+2
+
+var a = "Hii"
+var b = "JavaScript"
+a-b 
+NaN (not a number)
+a*b
+NaN
+a/b
+NaN
+a+b (concat)
+"HiiJavaScript"
+
+
+var a = 10
+var b = 20
+undefined
+b%a
+0
+
+2%4
+2
+3%4
+3
+4%4
+0
+5%4
+1
+7%4
+3
+100%101
+100
+
+5%4
+1
+7%4
+3
+6%4
+2
+8%4
+0
+
+var a = "Hii"
+undefined
+var b = 10
+undefined
+a+b
+"Hii10"
+a-b
+NaN
+a*b
+NaN
+
+var a = "Hii "
+undefined
+var b = "JavaScript"
+undefined
+a+b
+"Hii JavaScript"
+
+string+string = string (concat)
+string+number = string (concat)
+number+string = string (concat)
+number+number = number (addition)
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+"10"+20+30-1
+"102030"-1
+102029
+10+"20"+30-1
+"102030"-1
+102029
+10+20+"30"-1
+"3030"-1
+3029
+
+
+"10a"-1
+NaN
+"20"-1
+19
+
+"10a"-1
+NaN
+2-"10"
+-8
+2*"10"
+20
+2+"10"
+"210"
+"10"-"20"
+-10
+"10"-"20a"
+NaN
---
+++ javascript/second.js
@@ -0,0 +1,156 @@
+var a = true
+var b = false
+var c = true
+
+a+c
+a+b
+
+a+b
+1
+a+c
+2
+
+true+true
+1+1 
+2
+
+true is equal 1
+false is equal 0
+
+var a = true
+var b = false
+var c = true
+undefined
+10+a
+11
+10-a
+9
+10+b
+10
+10-b
+10
+10/b
+Infinity
+var a = "Hii "
+undefined
+a+b
+"Hii false"
+a-c
+NaN
+
+
+var a = 'true'
+var b = "false"
+var c = "true"
+
+a-b
+NaN
+
+
+a+b
+"truefalse"
+"true"+"true"
+"truetrue"
+"true"-10
+NaN
+10-true
+9
+
+var a = 10
+var b = "Hi"
+
+typeof(a)
+"number"
+typeof(a)
+"number"
+typeof(b)
+"string"
+var c = true
+typeof(c)
+"boolean"
+var d = "10"
+typeof(d)
+"string"
+var e = "true"
+typeof(e)
+"string"
+
+"10"+5 =15
+"105"
+
+var a = "10"
+var b = "20"
+a+b
+"1020"
+parseInt(a)+parseInt(b)
+30
+parseInt(a)
+10
+
+var a = "101abc11"
+parseInt(a)
+101
+
+var a = "abc11"
+parseInt(a)
+NaN
+
+var c ="abc"
+parseInt(c)
+NaN
+
+var a = "10.11"
+var b = "20.23"
+
+//es5
+parseInt(a)+parseInt(b)
+30
+parseFloat(a)+parseFloat(b)
+30.34
+
+var a = "10.11aaa10"
+parseFloat(a)
+10.11
+var a = "a10.11aaa10"
+parseFloat(a)
+NaN
+var a = "10a.1110"
+
+parseFloat(a)
+10
+
+//es6
+Number
+
+var a = "10.11"
+Number(a)
+10.11
+
+var a = "10"
+Number(a)
+10
+var a = "10a"
+Number(a)
+NaN
+
+var a = "10a11"
+Number(a)
+NaN
+
+prompt("what is your name")
+"aakash"
+var a = prompt("what is your name")
+undefined
+a
+"aakash"
+
+
+var a = prompt("Enter first number")
+var b = prompt("Enter second number")
+Number(a)+Number(b)
+30
+
+var a = prompt("Enter first number")
+var b = prompt("Enter second number")
+Number(a)-Number(b)
+10
